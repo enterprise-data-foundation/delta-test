0de4899a23ff3f18458a1b10c1245e72768a2941
Jason Williscroft <jwilliscroft@continuus-technologies.com>
2019-01-10 22:18:01 +0000

    Updated test to call Show-Execution

--- Test/HelloWorld.ps1
+++ Test/HelloWorld.ps1
@@ -1,5 +1,5 @@
 # Load & configure deltaTest.
 Invoke-Expression "$(Get-ItemPropertyValue -Path "HKCU:\Software\EnterpriseTestFoundation\deltaTest" -Name "ModuleDir")\config.ps1"
 
-# Return result to calling script.
-return "BANG!"
+# Display result & pause unless $Global:NoInput is set.
+Show-Execution -Message "Hello, World!"
