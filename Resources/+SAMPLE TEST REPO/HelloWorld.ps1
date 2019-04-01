35480980f38bf76435a5fc1202f1f260779d4e00
Jason Williscroft <jwilliscroft@continuus-technologies.com>
2019-04-01 19:00:48 -0400

    Refactored to support 2.0. Interim Commit.

--- Resources/+SAMPLE TEST REPO/HelloWorld.ps1
+++ Resources/+SAMPLE TEST REPO/HelloWorld.ps1
@@ -1,5 +1,9 @@
 # Load & configure deltaTest.
-Invoke-Expression "$(Get-ItemPropertyValue -Path "HKLM:\Software\EnterpriseDataFoundation\deltaTest" -Name "ModuleDir")\config.ps1"
+Invoke-Expression "$env:deltaTest\init.ps1"
 
 # Display result & pause unless $Global:NoInput is set.
-Show-Execution -Message "Hello, World!"
+# Show-Execution -Message "Hello, World!"
+
+$deltaTestConfig.NoInput
+
+Read-Host "Press Enter"
