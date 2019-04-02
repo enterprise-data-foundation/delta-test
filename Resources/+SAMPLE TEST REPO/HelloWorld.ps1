d698df903c08ed8dcc38c3df9e6235cb62acf9e1
Jason Williscroft <jwilliscroft@continuus-technologies.com>
2019-04-02 18:30:48 -0400

    Renamed sample test repo

--- Resources/+SAMPLE TEST REPO/HelloWorld.ps1
+++
@@ -1,9 +0,0 @@
-# Load & configure deltaTest.
-Invoke-Expression "$env:deltaTest\init.ps1"
-
-# Display result & pause unless $Global:NoInput is set.
-# Show-Execution -Message "Hello, World!"
-
-$deltaTestConfig.NoInput
-
-Read-Host "Press Enter"
